71babbbe9c85eff4feaba67cdca67a8b1ae92aed
Debasish Borah <78498746+DebasishBorah133@users.noreply.github.com>
2023-02-16 18:30:54 +0530

    Add files via upload

---
+++ CI Platform/Controllers/landingPage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform.Controllers
+{
+    public class landingPage : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MissionPage()
+        {
+            return View("MissionPage");
+        }
+    }
+}
